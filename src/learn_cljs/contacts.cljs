d8a84890d1d006d6eef19960de7d7da4e6f07cc7
MityaDementiy <d.dementiy@gmail.com>
2022-11-07 19:19:41 +0200

    Add rendering contact func and open contact event listener

--- src/learn_cljs/contacts.cljs
+++ src/learn_cljs/contacts.cljs
@@ -44,10 +44,38 @@
 
 (def app-container (gdom/getElement "app"))
 
-(defn attach-event-handlers! [state])
-
 (defn set-app-html! [html-str]
   (set! (.-innerHTML app-container) html-str))
+
+(defn format-name [contact]
+  (->> contact
+       ((juxt :first-name :last-name))
+       (str/join " ")))
+
+(defn delete-icon [idx]
+  [:span {:class "delete-icon"
+          :data-idx idx}
+   [:span {:class "mu mu-delete"}]])
+
+(defn render-contact-list-item [idx contact selected?]
+  [:div {:class (str "card contact-summary" (when selected? " selected"))
+         :data-idx idx}
+   [:div {:class "card-content"}
+    [:div {:class "level"}
+     [:div {:class "level-left"}
+      [:div {:class "level-item"}
+       (delete-icon idx)
+       (format-name contact)]]
+     [:div {:class "level-right"}
+      [:span {:class "mu mu-right"}]]]]])
+
+(defn on-open-contact [e state]
+  (refresh!
+   (let [idx (int (.. e -currentTarget -dataset -idx))]
+     (assoc state :selected? idx
+                  :editing? true))))
+
+(defn attach-event-handlers! [state])
 
 (defn render-app! [state]
   (set-app-html! (hiccups/html [:div "Hello"])))
@@ -57,8 +85,3 @@
   (attach-event-handlers! state))
 
 (refresh! initial-state)
-
-(defn format-name [contact]
-  (->> contact
-       ((juxt :first-name :last-name))
-       (str/join " ")))
